82bfc809f315a1d7d0b7a79c2d5c1f34afc2e0e7
amdudda <tk0654wm@minneapolis.edu>
2016-04-15 09:24:53 -0500

    Add config for passport implementation

---
+++ config/auth.js
@@ -0,0 +1,10 @@
+// TODO SET ENV VARS
+
+module.exports = {
+	'twitterAuth' : {
+		'consumerKey': process.env.PTWIT_CONSUMER_KEY,
+		'consumerSecret': process.env.PTWIT_CONSUMER_SECRET,
+		'callbackURL' : 'http://127.0.0.1:3000/auth/twitter/callback'
+	}
+
+};
